f8d7c13936d717797150a4a3b13ba6ac0c3af966
Alisa Poon <alisa.poon97@gmail.com>
2020-12-29 19:00:05 -0800

    corrected alisaRunLengthEncoding

--- easyInterviewQuestions/runLengthEncoding/alisaRunLengthEncoding.js
+++ easyInterviewQuestions/runLengthEncoding/alisaRunLengthEncoding.js
@@ -3,31 +3,27 @@ function runLengthEncoding(string) {
   let arr = [];
   const hash = {};
   for (let i = 0; i < string.length; i++) {
-    if (hash[string[i]]) {
-      hash[string[i]]++;
-    } else {
+    if (!hash[string[i]]) {
       hash[string[i]] = 1
+    } else {
+      hash[string[i]]++
     }
-    if (i === string.length - 1) {
-      console.log(hash);
+    if (string[i] !== string[i+1]) {
       for (const [key, value] of Object.entries(hash)) {
         if (value > 9) {
-          for (let i = value; i > 0; i-=9) {
-            if (i < 9) {
-              arr.push(`${i}${key}`);
-            } else {
-              arr.push(`9${key}`);
-            }
+          for (let i = value; i > 0; i -= 9) {
+            (i < 9) ? arr.push(`${i}${key}`) : arr.push(`9${key}`);
           }
         } else {
           arr.push(`${value}${key}`);
         }
       }
+      delete hash[string[i]];
     }
   }
   return arr.join('');
 }
 
-const string = "AAAAAAAAAAAAABBCCCCDD"
+const string = "AAABBAABCCCCCCCCCCCCCCCCCCCCCCCCCDDD"
 
 console.log(runLengthEncoding(string));
