023558fe0829ae95579cd719f0c42c14126eda23
sotrnguy92 <sonnguyen092@gmail.com>
2021-01-19 12:31:06 -0800

    committing solution to runLengthEncoding

--- easyInterviewQuestions/runLengthEncoding/sonSolution.js
+++ easyInterviewQuestions/runLengthEncoding/sonSolution.js
@@ -1,7 +1,22 @@
 function runLengthEncoding(string) {
-    const encoding = []
+    const encoding = ["1"+string[0]]
 
-    for (let i =0; i < string.length; i++) {
-
+    for (let i =1; i < string.length; i++) {
+        let count = parseInt(encoding[encoding.length-1][0]);
+        if (string[i] !== encoding[encoding.length-1][1] || encoding[encoding.length-1][0] === "9"){
+            count = 1;
+            encoding.push(count.toString()+string[i])
+        }else if(string[i] === encoding[encoding.length-1][1]){
+            count++;
+            encoding[encoding.length-1] = count.toString()+string[i];
+            console.log(encoding)
+        }
     }
+    return encoding.join("");
 }
+
+
+const str = ";;;;;;;;;;;;''''''''''''''''''''1233333332222211112222111s"
+
+runLengthEncoding(str);
+console.log(runLengthEncoding(str))
