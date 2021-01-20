d6f743cb111477db84b13552c74a6d8871f8c35c
Alisa Poon <alisa.poon97@gmail.com>
2021-01-19 18:31:07 -0800

    alisa's bubble sort

---
+++ easyInterviewQuestions/bubbleSort/alisaBubbleSort.js
@@ -0,0 +1,19 @@
+function bubbleSort(array) {
+  // Write your code here.
+  let bubble = [...array];
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] > array[i+1]) {
+      array.splice(i, 2, array[i+1], array[i])
+    }
+  }
+  if (bubble.join('') === array.join('')) {
+    return bubble;
+    } else {
+    return bubbleSort(array)
+  }
+
+}
+
+let array = [8, 5, 2, 9, 5, 6, 3];
+
+console.log(bubbleSort(array));
