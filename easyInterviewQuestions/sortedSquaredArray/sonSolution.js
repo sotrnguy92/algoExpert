52c26897bb44ea8a89dc369ad6a8cbdc682b94aa
sotrnguy92 <sonnguyen092@gmail.com>
2021-08-13 11:05:25 -0700

    adding son solution to sorted squared array

--- easyInterviewQuestions/sortedSquaredArray/sonSolution.js
+++ easyInterviewQuestions/sortedSquaredArray/sonSolution.js
@@ -1,4 +1,12 @@
 function sortedSquaredArray(array) {
   // Write your code here.
-  return [];
+  return array.map(x => x**2).sort((a,b) => a-b);
 }
+
+// function sortedSquaredArray(array) {
+//   // Write your code here.
+//   for (let i = 0; i < array.length; i++) {
+//     array[i] *= array[i]
+//   }
+//   return array.sort((a,b) => a - b);
+// }
