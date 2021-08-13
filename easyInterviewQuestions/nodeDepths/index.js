c8a2edcbe580ebed2bc0e97c37034cce9368dcdc
sotrnguy92 <sonnguyen092@gmail.com>
2021-08-13 13:11:13 -0700

    refactoring index in node depths to default code

--- easyInterviewQuestions/nodeDepths/index.js
+++ easyInterviewQuestions/nodeDepths/index.js
@@ -1,11 +1,3 @@
-function nodeDepths(root, depth=0) {
-    let totalSum = 0;
-    if (root.left){
-        totalSum += nodeDepths(root.left, depth +1);
-    }
-    if (root.right){
-        totalSum += nodeDepth(root.right, depth + 1);
-    }
-    totalSum += depth;
-    return totalSum;
+function nodeDepths(root) {
+    // Write your code here.
 }
